146e6a2360537d95e039fcfd8ff8ee38d047fe0a
shafaetjsr <shafaet811@gmail.com>
2020-06-22 11:51:06 +0600

    Variables and Data Type

---
+++ Variables.py
@@ -0,0 +1,21 @@
+x= 5  #x is of type int
+
+y='shafaet'  # x is now of type str
+
+a= 5.5
+
+b = True;
+
+print(x)
+
+print(y)
+
+# print Data Type
+
+print(type(x))
+
+print(type(y))
+
+print(type(a))
+
+print(type(b))
