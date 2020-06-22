1b85ffb297acd247fae8a439040484c7d1cc681b
shafaetjsr <shafaet811@gmail.com>
2020-06-22 12:14:50 +0600

    number and casting complate

---
+++ Number.py
@@ -0,0 +1,56 @@
+# int
+# float 
+# complex
+
+
+x =5
+
+y =5.5
+
+z = 5j
+
+
+
+
+print(x)
+print(y)
+print(z)
+
+
+print(type(x))
+
+print(type(y))
+
+
+print(type(z))
+
+
+# Python int casting
+a = int(1)
+
+b = int(2.6)
+
+c = int('3')
+print(a)
+print(b)
+print(c)
+
+# Python float casting
+
+p = float(1)
+q =float(2.8)
+r = float('3')
+s = float('3.9')
+print(p)
+print(q)
+print(r)
+print(s)
+
+# Python string or str casting
+
+m = str(2)
+n = str('sh1')
+o = str(50.5)
+print(m)
+print(n)
+print(0)
--- README.md
+++ README.md
@@ -7,4 +7,8 @@ Python Basic Information
 Go => Run menu, select => Run Without Debugging or Ctrl+F5
 
 # Comments 
-Use '#' for Single Line Comment and Three (""") Qutation for Multiple Comments 
+Use '#' for Single Line Comment and Three (""") Qutation for Multiple Comments
+
+# Git Quick Link
+
+[Variable Section ](https://github.com/shafaetjsr/python3.8.3/blob/master/Variables.py)
