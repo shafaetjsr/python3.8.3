e383b0edda882c5acbc64c9e40c9325ae9cdedd6
shafaetjsr <shafaet811@gmail.com>
2020-06-24 18:14:57 +0600

    Lambda and array complated

---
+++ Array.py
@@ -0,0 +1,21 @@
+cars = ["Ford", "Volvo", "BMW"]
+
+print(cars)
+
+
+cars = ["Ford", "Volvo", "BMW"]
+
+cars[0] = "Toyota"
+
+print(cars)
+
+x = len(cars)
+print(x)
+
+
+cars.append("Honda")
+
+print(cars)
+
+cars.remove("Volvo") 
+print(cars)
