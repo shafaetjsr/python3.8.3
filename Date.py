4523602a3e5f75486105694dd5bc720ba868510a
shafaetjsr <shafaet811@gmail.com>
2020-06-27 17:37:11 +0600

    Date Finished

---
+++ Date.py
@@ -0,0 +1,21 @@
+import datetime
+
+x = datetime.datetime.now()
+
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x) 
+
+print(x)
+
+print(x.year)
+
+print(x.strftime('%A'))
+print(x.strftime('%B'))
+
+print(x.strftime('%a'))
+print(x.strftime('%b'))
+print(x.strftime('%x'))
+
+print(x.strftime('%p'))
